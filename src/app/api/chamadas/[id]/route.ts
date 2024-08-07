9fa524c8afb3acfe369961fc8a20d2c8005510c5
Jonas Souza Alves <alves.souza.jonas@gmail.com>
2024-08-07 00:10:21 -0300

    ajustado a variavel horachamado na atualização da chamada

--- src/app/api/chamadas/[id]/route.ts
+++ src/app/api/chamadas/[id]/route.ts
@@ -1,4 +1,3 @@
- // Ajuste o caminho conforme necessário
 import { NextResponse } from 'next/server';
 import { unstable_noStore as noStore } from 'next/cache';
 import supabase from '../../../../../utils/supabase-connect';
@@ -8,7 +7,8 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
   const { id } = params;
   const { paciente, atendimento, sala, prioritario, data, identificacao, status, horaChamado } = await req.json();
 
-  // Realiza o UPDATE diretamente com o Supabase
+  var horachamado = horaChamado
+
   const { data: updateData, error: updateError } = await supabase
     .from('chamadas')
     .update({
@@ -19,13 +19,13 @@ export async function PUT(req: Request, { params }: { params: { id: string } }) 
       data,
       identificacao,
       status,
-      horaChamado
+      horachamado
     })
     .eq('id', id);
 
   if (updateError) {
     return new Response(JSON.stringify({ message: 'Erro ao atualizar chamada', error: updateError.message }), { status: 500 });
   }
-
+  
   return new Response(JSON.stringify({ message: 'Chamada atualizada com sucesso' }), { status: 200 });
 }
